7b196c3c72958efd876a9a2cdda1c00dffe8579e
Brendan Deere <brendan.deere@gmail.com>
2025-10-31 16:40:47 -0400

    basic env

---
+++ flake.nix
@@ -0,0 +1,39 @@
+{
+  description = "Ruby Web Application";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable"; 
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+
+        ruby = pkgs.ruby_3_4; 
+        nodejs = pkgs.nodejs_22;
+      in {
+        devShells.default = pkgs.mkShell {
+          name = "ruby-node-dev-shell";
+
+          # Native dependencies for building gems and node modules
+          buildInputs = with pkgs; [
+            ruby
+            pnpm
+            git
+          ];
+
+          shellHook = ''
+            # Activate the Ruby environment (Bundler-installed gems)
+            export PATH="$GEM_HOME/bin:$PATH"
+
+            echo "🔧 Ruby + Node.js development shell"
+            echo "Ruby version: $(ruby -v)"
+            echo "Node version: $(node -v)"
+            echo "PNPM version: $(pnpm -v)"
+          '';
+        };
+      }
+    );
+}
